4fa5590fb63b0e76a5b3b77f9d7b673008dce02e
Bryan Roberts <roberts@amyris.com>
2023-08-24 08:45:44 -0700

    tokyo night theme

--- init.lua
+++ init.lua
@@ -130,12 +130,21 @@ require('lazy').setup({
     },
   },
 
+--  {
+--    -- Theme inspired by Atom
+--    'navarasu/onedark.nvim',
+--    priority = 1000,
+--    config = function()
+--      vim.cmd.colorscheme 'onedark'
+--    end,
+--  },
+
   {
-    -- Theme inspired by Atom
-    'navarasu/onedark.nvim',
-    priority = 1000,
+  "folke/tokyonight.nvim",
+  lazy = false,
+  priority = 1000,
     config = function()
-      vim.cmd.colorscheme 'onedark'
+      vim.cmd.colorscheme 'tokyonight-moon'
     end,
   },
 
